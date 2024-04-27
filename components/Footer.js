31c997b471a355e31c9b5497648029315cd8a385
Ashish Kumar <ashish93343@gmail.com>
2024-04-27 23:03:39 +0530

    changed tree to hub

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer aria-label="Site Footer" className=" fixed bottom-5 left-1/2 -translate-x-1/2">
       <Link className='flex flex-row items-center transition-all duration-400 '  target='_blank' href='/'>
         <img className='hover:-rotate-45' src='/images/favicon.ico' />
-        <h5 className='text-indigo-400 pl-3 font-bold hover:text-indigo-300 '> Try Linktree</h5>
+        <h5 className='text-indigo-400 pl-3 font-bold hover:text-indigo-300 '> Try LinkHub</h5>
       </Link>
    
     </footer>

cb7dd506fe9a05222632dfba182ebdc3a41df965
Ashish Kumar <ashish93343@gmail.com>
2024-04-27 22:43:20 +0530

    made changes

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ export default function Home() {
       />
       
       <Apply />
-      {/* <main className="w-full min-h-screen flex flex-col justify-center items-center">
-        <h1 className='text-center'> Welcome to <br/><span className='text-indigo-600 font-semibold'>LinkTree</span></h1>
-        <Link title='Notice the page loader' className='bg-indigo-600 rounded-sm inline-block my-2 p-1 px-2 text-white' href="/apply">Link to a page</Link>
-      </main> */}
+     
     </>
   )
 }
